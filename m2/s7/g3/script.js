07d5f3ede64798a5d2e0b5367f8b515df3f683a0
Deli-97 <fumantifederica.ff@gmail.com>
2023-11-09 15:45:29 +0100

    commit

--- m2/s7/g3/script.js
+++ m2/s7/g3/script.js
@@ -1,7 +1,35 @@
+let cartArray = [];
+
 fetch(' https://striveschool-api.herokuapp.com/books')
 .then(res => res.json())
 .then(books => {
+
+    let target = document.querySelector('#target');
+
+    books.forEach(book => {
+        let clone = getCardClone();
+        let img = clone.querySelector('.card-img-top');
+        let title = clone.querySelector('.card-title');
+        let price = clone.querySelector('.card-price');
+        let addToCart = clone.querySelector('.AddToCart');
+
+        img.src = book.img;
+        title.innerText = book.title;
+        price.innerText = book.price;
+        
+        addToCart.addEventListener('click', function(){
+            cartArray.push(book);
+            localStorage.setItem('cart', JSON.stringify(cartArray))
+        })
+
+        target.append(clone);
+    })
     
-    
+
 });
+
+function getCardClone(){
+    let template = document.getElementsByTagName('template')[0];
+    return template.content.cloneNode(true)
+}
 
